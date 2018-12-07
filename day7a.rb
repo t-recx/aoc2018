41d54f4912c3f089203d03000e8896a2be9f5ae8
João Bruno <bruno.joao@live.com.pt>
2018-12-07 23:27:07 +0000

    Added day7a

---
+++ day7a.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+def select steps
+  steps.select { |x| steps.none? { |y| y[1] == x[0] } }.map { |x| x[0] }.sort
+end
+
+steps = File.readlines(ARGV[0]).map {|l| l.split(' ') }.map { |t| [t[1], t[7]] }
+
+requirements = {}
+
+steps.each do |requirement, step|
+  requirements[step] = [] unless requirements[step]
+  requirements[step].push requirement
+end
+
+steps.select { |x| requirements.keys.none? { |k| k == x[0] } }.map { |x| x[0] }.each { |step| requirements[step] = [] }
+
+while next_step = requirements.select { |k,v| v.count == 0 }.sort.map { |x| x[0] }.first do
+  requirements.each { |k, v| v.delete next_step }
+
+  requirements.delete(next_step)
+
+  putc next_step
+end
